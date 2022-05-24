c7cd1d560295188535d22eb8a08cbd3c696bea49
Martin Betancur <martinbetancur27@gmail.com>
2022-05-24 15:51:56 -0500

    Add http and print JSON by console

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+
+
+Console.WriteLine("Input the word");
+string word = Console.ReadLine();
+
+string url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+HttpClient client = new HttpClient();
+
+var httpResponse = await client.GetAsync(url + word);
+
+if (httpResponse.IsSuccessStatusCode)
+{
+    var content = await httpResponse.Content.ReadAsStringAsync();
+
+    Console.WriteLine(content);
+}
