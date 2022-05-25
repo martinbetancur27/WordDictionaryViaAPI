a22de9af39a9d5b402b45c43b406494ee17d90b1
Martin Betancur <martinbetancur27@gmail.com>
2022-05-25 11:31:25 -0500

    Add try-catch httpResponse

--- Program.cs
+++ Program.cs
@@ -19,61 +19,68 @@ while (makeSearch)
     Console.WriteLine("> Input the word: ");
     wordToSearch = Console.ReadLine();
 
-    httpResponse = await client.GetAsync(urlApi + wordToSearch);
-
-    if (httpResponse.IsSuccessStatusCode)
+    try
     {
-        
-        string finalContent = "WORD: " + wordToSearch.ToUpper() + "\n";
-        var content = await httpResponse.Content.ReadAsStringAsync();
+        httpResponse = await client.GetAsync(urlApi + wordToSearch);
 
-        //Enable case-insensitive property name matching with System.Text.Json
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        };
-
-        List<Word> resultWord =
-        JsonSerializer.Deserialize<List<Word>>(content, options);
-
-        //Logic API: The first element are the definitions
-        var firstElement = resultWord.First();
-        var listMeaning = firstElement.Meanings;
-        var listDefinitions = listMeaning[0].Definitions;
-
-        foreach (var word in listDefinitions)
+        if (httpResponse.IsSuccessStatusCode)
         {
             
-            finalContent += title + "--> " + word.definition + "\n" + "--> " + word.Example + "\n";
+            string finalContent = "WORD: " + wordToSearch.ToUpper() + "\n";
+            var content = await httpResponse.Content.ReadAsStringAsync();
 
+            //Enable case-insensitive property name matching with System.Text.Json
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Word> resultWord =
+            JsonSerializer.Deserialize<List<Word>>(content, options);
+
+            //Logic API: The first element are the definitions
+            var firstElement = resultWord.First();
+            var listMeaning = firstElement.Meanings;
+            var listDefinitions = listMeaning[0].Definitions;
+
+            foreach (var word in listDefinitions)
+            {
+                
+                finalContent += title + "--> " + word.definition + "\n" + "--> " + word.Example + "\n";
+
+            }
+
+            Console.WriteLine(finalContent);
+
+            Console.WriteLine("\n*** Congratulations, You know the meaning of " + wordToSearch + " ***");
+
+            Console.WriteLine("\n> Do you want to save your search to a file? Enter y/n");
+            saveSearch = Console.ReadLine() == "y" ? true : false;
+
+            if(saveSearch)
+            {
+                try
+                {
+                    await File.WriteAllTextAsync(wordToSearch + ".txt", finalContent);
+                    Console.WriteLine("***** Saved *****");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sorry, can't save: " + ex.Message);
+                }
+            }
         }
-
-        Console.WriteLine(finalContent);
-
-        Console.WriteLine("\n*** Congratulations, You know the meaning of " + wordToSearch + " ***");
-
-        Console.WriteLine("\n> Do you want to save your search to a file? Enter y/n");
-        saveSearch = Console.ReadLine() == "y" ? true : false;
-
-        if(saveSearch)
+        else
         {
-            try
-            {
-            await File.WriteAllTextAsync(wordToSearch + ".txt", finalContent);
-            Console.WriteLine("***** Saved *****");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Sorry, can't save: " + ex.Message);
-            }
+            Console.WriteLine("The word "+ wordToSearch + " is not in our dictionary.");
         }
-    }
-    else
-    {
-        Console.WriteLine("The word "+ wordToSearch + " is not in our dictionary.");
-    }
     
-    Console.WriteLine("\n> Do you want to perform another search? Enter y/n");
-    makeSearch = Console.ReadLine() == "y" ? true : false;
-
+        Console.WriteLine("\n> Do you want to perform another search? Enter y/n");
+        makeSearch = Console.ReadLine() == "y" ? true : false;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Sorry, the program not work. " + ex.Message);
+        makeSearch = false;
+    }
 }
